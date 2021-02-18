5db2ebb602dcb42c4d92c5c7182f7eacf33e7cbb
Eion Nelson <ecnelson1@live.com>
2021-02-18 15:21:34 -0800

    added heroku remotes and github ci

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { hellCast } = require('./data.js');
+app.use(cors());
+
+app.get('/cast', (req,res) => {
+    res.json({hellCast});
+});
+app.get('/cast:name', (req,res) => {
+    const name = Number(req.params.id);
+    const chosenCast = hellCast.find(castMember => castMember.id===id);
+    res.json({chosenCast})
+});
+module.exports = { app }
